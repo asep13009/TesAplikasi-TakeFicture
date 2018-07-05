6c28691f7622dad84b3c25a69966b2b283111deb
Asep Sudrajat <sudrajatsep16@gmail.com>
2018-07-05 19:28:06 +0700

    video and maps

--- app/src/main/java/com/example/asep/tesaplikasi/TesActivity.java
+++ app/src/main/java/com/example/asep/tesaplikasi/TesActivity.java
@@ -14,13 +14,15 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.VideoView;
 
 import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class TesActivity extends AppCompatActivity implements View.OnClickListener {
-    Button camera;
+    Button camera, btnvideo;
+    VideoView video;
     ImageView gambar;
     static  int media_image=100;
     static  int media_video=200;
@@ -42,8 +44,23 @@ public class TesActivity extends AppCompatActivity implements View.OnClickListen
 
         camera= findViewById(R.id.button3);
         gambar = findViewById(R.id.imageView);
+        btnvideo = findViewById(R.id.btnvideo);
+        video = findViewById(R.id.videoView);
         camera.setOnClickListener(this);
+        btnvideo.setOnClickListener(this);
 
+
+        Button kemaps = findViewById(R.id.buttonMaps);
+        kemaps.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent= new Intent(getApplicationContext(),MapsActivity.class);
+
+                startActivity(intent);
+
+            }
+        });
 
     }
 
@@ -51,6 +68,8 @@ public class TesActivity extends AppCompatActivity implements View.OnClickListen
     public void onClick(View view) {
         if(view==camera){
             CaptureImage();
+        }else if(view == btnvideo){
+            RecordVideo();
         }
     }
     public void CaptureImage(){
@@ -58,6 +77,13 @@ public class TesActivity extends AppCompatActivity implements View.OnClickListen
         this.fileUri = getMediaFileUri(media_image);
         intent.putExtra("output",this.fileUri);
         startActivityForResult(intent,request_image);
+    }
+
+    public void RecordVideo(){
+        Intent intent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+        this.fileUri = getMediaFileUri(media_video);
+        intent.putExtra("output",this.fileUri);
+        startActivityForResult(intent,request_video);
     }
     public Uri getMediaFileUri(int meditype){
         return Uri.fromFile(getFileUri(meditype));
@@ -67,19 +93,35 @@ public class TesActivity extends AppCompatActivity implements View.OnClickListen
         if (mediaStorage.exists()||mediaStorage.mkdirs()){
             String timesmap = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
             if (mediatype==media_image){
-                return new File(mediaStorage.getPath() +File.separator + "IMG" + timesmap + ".jpg");
+                return new File(mediaStorage.getPath() +File.separator + "IMG_" + timesmap + ".jpg");
+            }else{
+                return new File(mediaStorage.getPath() +File.separator + "VID_" + timesmap + ".mp4");
             }
-            return null;
         }
         Log.d("error","gagal membuat directory");
         return null;
     }
     public  void onActivityResult(int requestCode,int result_code,Intent data){
         if (requestCode == request_image){
-            gambar.setImageBitmap(BitmapFactory.decodeFile(this.fileUri.getPath()));
-        }else {
-            Toast.makeText(getApplicationContext(),"user Cancled Img Capture",Toast.LENGTH_SHORT).show();
+            if(result_code == RESULT_OK){
+                gambar.setVisibility(View.VISIBLE);
+                video.setVisibility(View.INVISIBLE);
+                gambar.setImageBitmap(BitmapFactory.decodeFile(this.fileUri.getPath()));
+            }else {
+                Toast.makeText(getApplicationContext(), "user Cancled Img Capture", Toast.LENGTH_SHORT).show();
+            }
+        }else if(requestCode == request_video){
+            if(result_code == RESULT_OK){
+                gambar.setVisibility(View.INVISIBLE);
+                video.setVisibility(View.VISIBLE);
+                video.setVideoPath(this.fileUri.getPath());
+                video.start();
+            }else{
+                Toast.makeText(getApplicationContext(), "user Canceled Vid Capture", Toast.LENGTH_SHORT).show();
+            }
         }
+
+
     }
 
 
